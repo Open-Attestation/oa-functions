f2d0447e4a75f718bfe69d17769c17fc76036b15
rjchow <me@rjchow.com>
2021-02-24 11:10:24 +0800

    wip

--- src/faucet/index.js
+++ src/faucet/index.js
@@ -4,6 +4,12 @@ const config = require("./config");
 
 const handleFaucet = async (event, _context, callback) => {
   try {
+    
+    // step 1: handle the request, extract the target wallet address
+    // step 2: unlock /your own wallet/ -- wallet private key should be provided in env var
+    // step 3: transfer 1 eth to the target wallet address
+    // step 4: send successful response
+
     callback(null, {
       statusCode: 200,
       headers: { "Content-Type": "application/json" },
