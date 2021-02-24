2efc7f28e6f5eb1434d1fc0420f5e995267933c1
ryansee <ryansee.sw@outlook.com>
2021-02-24 14:10:44 +0800

    feat: ether faucet

--- src/faucet/index.js
+++ src/faucet/index.js
@@ -1,20 +1,64 @@
 const middy = require("middy");
 const { cors } = require("middy/middlewares");
 const config = require("./config");
+const { ethers, utils } = require("ethers");
 
 const handleFaucet = async (event, _context, callback) => {
   try {
+    // const provider = ethers.getDefaultProvider("ropsten");
+    const provider = new ethers.providers.InfuraProvider("ropsten");
     
     // step 1: handle the request, extract the target wallet address
-    // step 2: unlock /your own wallet/ -- wallet private key should be provided in env var
-    // step 3: transfer 1 eth to the target wallet address
-    // step 4: send successful response
+    // const receiver = event.pathParameters.walletAddress; // TODO: validate wallet address
+    const receiver = "0x709731d94D65b078496937655582401157c8A640";
 
-    callback(null, {
-      statusCode: 200,
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({"response": "OK"})
-    });
+    if (!utils.isAddress(receiver)) {
+      throw new Error("Invalid wallet address");
+    } else {
+      // step 2: unlock /your own wallet/ -- wallet private key should be provided in env var
+      const privateKey = process.env.FAUCET_PRIVATE_KEY;
+      const sender = new ethers.Wallet(privateKey, provider);
+
+      // provider.getBalance(sender.address).then((balance) => {
+      //   console.log("BALANCE: " + balance);
+      // })
+
+      // const balance = await sender.getBalance();
+      console.log(sender.address);
+      const balance2 = await provider.getBalance(sender.address);
+      //0x3bd20c2493d07b2622455210ecdefe10e0713154
+      console.log(balance2);
+      // console.log(sender.getBalance());
+      // sender.getBalance().then((balance) => {
+      //   console.log("BALANCE: " + balance);
+      // })
+      
+
+      // const balance = sender.getBalance().then((balance) => {
+      //   console.log(balance);
+      // })
+      // balance.then((balance) => {
+      //   console.log(balance);
+      // });
+
+      // step 3: transfer 1 eth to the target wallet address
+      const transaction = {
+        to: receiver,
+        value: 1,
+        value: utils.parseEther("1"),
+      };
+
+      // const result = await sender.sendTransaction(transaction);
+      // console.log(result);
+      // step 4: send successful response
+      callback(null, {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({"response": "OK"})
+      });
+
+    }
+    
   } catch (e) {
     callback(null, {
       statusCode: 400,
