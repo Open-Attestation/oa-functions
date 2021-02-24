dc60bb74b0ffaef5a737bade74828ef73a7ff746
isaac <isaac.kps@gmail.com>
2021-02-24 17:00:46 +0800

    feat: update error status to 500 and error msg

--- src/faucet/index.js
+++ src/faucet/index.js
@@ -2,7 +2,6 @@ const middy = require("middy");
 const { cors } = require("middy/middlewares");
 const { ethers, utils } = require("ethers");
 const { AwsKmsSigner } = require("ethers-aws-kms-signer");
-const { reverse } = require("lodash");
 
 const handleFaucet = async (event, _context, callback) => {
   try {
@@ -13,13 +12,19 @@ const handleFaucet = async (event, _context, callback) => {
       keyId: process.env.KMS_KEY_ID,
     };
 
+    const receiver = event.pathParameters.walletAddress;
+    if (!utils.isAddress(receiver)) throw new Error("Invalid wallet address");
+
     const provider = ethers.getDefaultProvider("ropsten");
     let signer = new AwsKmsSigner(kmsCredentials);
     signer = signer.connect(provider);
-    const receiver = event.pathParameters.walletAddress;
-    if (!utils.isAddress(receiver)) throw new Error("Invalid wallet address");
     const signerBalance = ethers.utils.formatEther(await signer.getBalance());
-    if (signerBalance < 1) throw new Error("Insufficient Funds in wallet.");
+    if (signerBalance < 1) {
+      throw new Error(
+        "Oops! Faucet has ran dry, please inform the Open-Attestation team."
+      );
+    }
+
     const transfer = await signer.sendTransaction({
       to: receiver,
       value: utils.parseEther("1"),
@@ -35,7 +40,7 @@ const handleFaucet = async (event, _context, callback) => {
     });
   } catch (e) {
     callback(null, {
-      statusCode: 400,
+      statusCode: 500,
       body: e.message,
     });
   }
