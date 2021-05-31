06efccfba9e13bcf8f0a95ba88d59822e533f769
Sim Boon Long <simboonlong@gmail.com>
2021-05-31 18:56:50 +0800

    fix: correct variable

--- scripts/runE2eAgainstDeployment.sh
+++ scripts/runE2eAgainstDeployment.sh
@@ -3,8 +3,8 @@
 # Need to do this because each serverless.yml defines its own apigateway and hence has its own url
 # After iam dot notation update (https://www.serverless.com/framework/docs/deprecations#grouping-iam-settings-under-provideriam), need to remove first character, eg. awk was returning `=https://mehqrnf4bk.execute-api.ap-southeast-1.amazonaws.com/pr33`
 
-export VERIFY_ENDPOINT=`cd src/verify && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($0, 2) }'`
-export EMAIL_ENDPOINT=`cd src/email && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($0, 2) }'`
-export STORAGE_ENDPOINT=`cd src/storage && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($0, 2) }'`
+export VERIFY_ENDPOINT=`cd src/verify && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($2, 1) }'`
+export EMAIL_ENDPOINT=`cd src/email && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($2, 1) }'`
+export STORAGE_ENDPOINT=`cd src/storage && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($2, 1) }'`
 
 npm run e2etest
