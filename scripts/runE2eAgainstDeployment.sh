e8efe763f0f8966630d119b314406159a8c97e19
Sim Boon Long <simboonlong@gmail.com>
2021-05-31 18:47:20 +0800

    fix: remove first character

--- scripts/runE2eAgainstDeployment.sh
+++ scripts/runE2eAgainstDeployment.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 # Retrieve the endpoint from serverless info, only returns the first one for each
 # Need to do this because each serverless.yml defines its own apigateway and hence has its own url
-export VERIFY_ENDPOINT=`cd src/verify && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{print $2}'`
-export EMAIL_ENDPOINT=`cd src/email && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{print $2}'`
-export STORAGE_ENDPOINT=`cd src/storage && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{print $2}'`
+# After iam dot notation update (https://www.serverless.com/framework/docs/deprecations#grouping-iam-settings-under-provideriam), need to remove first character, eg. awk was returning `=https://mehqrnf4bk.execute-api.ap-southeast-1.amazonaws.com/pr33`
+
+export VERIFY_ENDPOINT=`cd src/verify && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($0, 2) }'`
+export EMAIL_ENDPOINT=`cd src/email && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($0, 2) }'`
+export STORAGE_ENDPOINT=`cd src/storage && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($0, 2) }'`
 
 npm run e2etest
