941972699e923aa227408cc05fd35a1ea91ee9d6
Sim Boon Long <simboonlong@gmail.com>
2021-06-01 10:43:50 +0800

    chore: log

--- scripts/runE2eAgainstDeployment.sh
+++ scripts/runE2eAgainstDeployment.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 # Retrieve the endpoint from serverless info, only returns the first one for each
 # Need to do this because each serverless.yml defines its own apigateway and hence has its own url
-# After iam dot notation update (https://www.serverless.com/framework/docs/deprecations#grouping-iam-settings-under-provideriam), need to remove first character, eg. awk was returning `=https://mehqrnf4bk.execute-api.ap-southeast-1.amazonaws.com/pr33`
 
-export VERIFY_ENDPOINT=`cd src/verify && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($2, 1) }'`
-export EMAIL_ENDPOINT=`cd src/email && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($2, 1) }'`
-export STORAGE_ENDPOINT=`cd src/storage && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($2, 1) }'`
+export VERIFY_ENDPOINT=`cd src/verify && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{print $2}'`
+export EMAIL_ENDPOINT=`cd src/email && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{print $2}'`
+export STORAGE_ENDPOINT=`cd src/storage && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{print $2}'`
 
-echo `cd src/storage && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{print $2}'`
-
-echo `cd src/storage && serverless info --verbose --stage $SEED_STAGE_NAME | awk '/ServiceEndpoint/{ print substr($2, 1) }'`
+echo `verify endpoint: $VERIFY_ENDPOINT`
+echo `email endpoint: $EMAIL_ENDPOINT`
+echo `storage endpoint: $STORAGE_ENDPOINT`
 
 npm run e2etest
